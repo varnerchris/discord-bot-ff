77ce254a4148690f7ba922c8cdb0cf5596015551
Chris <varnerchris@me.com>
2020-08-24 09:21:54 -0500

    restructured data integrity for multiple use cases

--- commands/trade.js
+++ commands/trade.js
@@ -6,7 +6,7 @@ const year = mflIds.Ids.year;
 const leagueID = mflIds.Ids.leagueID;
 const leagueOwners = mflIds.Ids.leagueOwners;
 
-const tradeBaitUrl = "http://www60.myfantasyleague.com/"+year+"/export?TYPE=tradeBait&L="+leagueID+"&APIKEY=&INCLUDE_DRAFT_PICKS=&JSON=1";
+const tradeBaitUrl = "http://www60.myfantasyleague.com/"+year+"/export?TYPE=tradeBait&L="+leagueID+"&APIKEY=&INCLUDE_DRAFT_PICKS=Y&JSON=1";
 const playerProfilesUrl = "http://www60.myfantasyleague.com/"+year+"/export?TYPE=players&L="+leagueID+"&APIKEY=&DETAILS=&SINCE=&PLAYERS=:playerIds&JSON=1"
 
 async function logFetch(url) {
@@ -48,29 +48,37 @@ const getData = async (user) => {
   let tradeResponse = await axios.get(tradeBaitUrl) //trade response
   let tradeBaitData = tradeResponse.data.tradeBaits.tradeBait // all trades
   let tradePlayerObject = filterData(tradeBaitData, user) // all players wanted in a trade
+  if(!tradePlayerObject){
+    throw new Error('no trade player object');
+  }
   let playerProfilesUrlFormatted = playerProfilesUrl.replace(':playerIds',tradePlayerObject.willGiveUp)
   let playerProfileResponse = await axios.get(playerProfilesUrlFormatted) //repsonse
+  let willGiveUp = playerProfileResponse.data.players.player;
 
   return {
-    willGiveUp: playerProfileResponse.data.players.player, // array of player objects
+    willGiveUp: Array.isArray(willGiveUp)?willGiveUp:[willGiveUp], // array of player objects
     inExchangeFor: tradePlayerObject.inExchangeFor //a string of inExhangeFor
   }
 }
+
 
 module.exports = {
   name: '!trade',
   description: 'Trade!',
     async execute(msg, args) {
-      console.log(msg)
       let mentionedUser = findMentionedUser(msg);
-      console.log(mentionedUser.id)
       let matchOwners = matchOwnersToMentions(mentionedUser.id,leagueOwners);
-      let returnTrades = await getData(matchOwners)
-      let playerNames = returnTrades.willGiveUp.map(playerObj =>{
-        return playerObj.name
-      })
+      let response;
+      try{
+        let returnTrades = await getData(matchOwners)
+        let playerNames = returnTrades.willGiveUp.map(playerObj =>{
+            return playerObj.name
+          }).join(',')
+        response =`${mentionedUser} is willing to trade: ${playerNames} and wants in return ${returnTrades.inExchangeFor} `
+      }catch(err){
+        response = 'No one available for trading'
+      }
 
-      let response =`${mentionedUser} will give up these players:${playerNames.join(' or ')} and wants in return ${returnTrades.inExchangeFor} `
       msg.channel.send(response);
   },
 };
